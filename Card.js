7a0bf4cfed9f7e613eaf41c14c03637633d89fea
Clay Bigelow <claybigelow@gmail.com>
2018-06-08 22:59:14 -0400

    added exports and changed constructors to Var

--- Card.js
+++ Card.js
@@ -1,23 +1,14 @@
 //constructor for Card object
 //input the suite h,d,s,c and rank 2-14
-function Card(suit, rank) {
+var Card =function(suit, rank) {
     this.Suit = suit;
     this.Rank = rank;
     this.Display = rank + suit;
-}
-
-//suit validation method that will return the valid suit or null
-function suitValidate(cardSuit) {
-    return (cardSuit === "h" || cardSuit === "d" || cardSuit === "s" || cardSuit === "c") ? cardSuit : null;
-}
-//rank validation methid that will return the valid rank or null
-function rankValidate(cardRank) {
-    return (cardRank >= 2 && cardRank <= 14) ? cardRank : null;
-}
+};
 
 //cardStack can function as a hand, deck, or community cards
 //It contains a card array and functions for moving cards in and out
-function cardStack() {
+var cardStack =function() {
 //array that contains all Card objects in the stack
     this.Cards = [];
     //method to fill Cards array with standard 52 count card deck
@@ -40,12 +31,7 @@ function cardStack() {
         var indexToReturn = Math.floor(Math.random() * topNum);
         return this.Cards.splice(indexToReturn,1);
     }
-}
+};
 
-
-var a = new cardStack;
-//a.fillDeck();
-console.log(a.Cards.length);
-console.log(a.dealRandom());
-console.log(a.Cards.length);
-console.log(a.Cards);
+exports.Cards=Card;
+exports.cardStack=cardStack;
