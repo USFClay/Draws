fcbc867ca7645fa7bd100d4684f9b9bb98822459
Clay Bigelow <claybigelow@gmail.com>
2018-06-08 21:13:58 -0400

    added validation function to Card constructor. Removed getter and setter fuctions as not needed.

--- Card.js
+++ Card.js
@@ -1,27 +1,25 @@
 //constructor for Card object
 //input the suite h,d,s,c and rank 2-14
-function Card(suite, rank) {
-    this.Suite= this.setSuite(suite);
-    this.Rank= this.setRank(rank);
-    this.Display=this.Suite + this.Rank.toString();
+function Card(suit, rank) {
+    this.Suit = suit;
+    this.Rank = rank;
+    this.Display = rank + suit;
 }
 
-//setter method for Suite
-Card.prototype.setSuite = function(cardSuite) {
-    //check to see if the suite is allowed
-        return (cardSuite==="h"|| cardSuite==="d" || cardSuite==="s" || cardSuite==="c") ? cardSuite : null;
-};
-//setter method for Rank
-Card.prototype.setRank = (cardRank) => {
-    //check to see if rank in range
-    return (cardRank>=2 && cardRank<=14) ? cardRank : null;
-};
 
-var a = new Card("h",2);
 
-console.log(a);
-console.log(a.Suite);
-console.log(a.Rank);
-console.log(a.Display);
+
+//suit validation method that will return the valid suit or null
+function suitValidate(cardSuit) {
+    return (cardSuit === "h" || cardSuit === "d" || cardSuit === "s" || cardSuit === "c") ? cardSuit : null;
+
+}
+//rank validation methid that will return the valid rank or null
+function rankValidate(cardRank) {
+    return (cardRank >= 2 && cardRank <= 14) ? cardRank : null;
+
+}
+
+
 
 
