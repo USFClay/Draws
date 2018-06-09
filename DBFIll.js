301aa90e14391702ffc6feb9bbb985510995379a
Clay Bigelow <claybigelow@gmail.com>
2018-06-09 15:34:04 -0400

    DB functional, inserting dummy data

--- DBFIll.js
+++ DBFIll.js
@@ -3,7 +3,47 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//pull in required files
 const Card = require("./Card");
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL for database
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'Draws';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    
+    const db = client.db(dbName);
+    insertDocuments(db, function () {
+        client.close();
+    });
+
+});
+
+//define insert function for inserting into specified collection
+const insertDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('testDraw');
+    // Insert some documents
+    collection.insertMany([
+        {a: 1}, {a: 2}, {a: 3}
+    ], function (err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+};
+
+
 
 
 //deal some hands 
